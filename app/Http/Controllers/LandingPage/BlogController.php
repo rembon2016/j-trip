51c2a8912aaf37b0bf594e1f61d911efb2b4120f
Ferdinalaxewall <86649049+ferdinalaxewall@users.noreply.github.com>
2024-06-12 21:42:31 -0700

    Enhancement/Landing Page (Blog): Implement Blog Visitor when Access Detail Blog Page

--- app/Http/Controllers/LandingPage/BlogController.php
+++ app/Http/Controllers/LandingPage/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\LandingPage;
 
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use App\Helpers\Utilities\Visitor;
 use App\Http\Controllers\Controller;
 
 class BlogController extends Controller
@@ -22,6 +23,9 @@ class BlogController extends Controller
         abort_if(is_null($data), 404);
 
         $blogs = Blog::where('slug', '!=', $slug)->limit(5)->orderBy('total_visitor', 'DESC')->get();
+        
+        // Record Visitor
+        Visitor::recordVisitor($data);
 
         return view('landing-page.pages.blog.detail', compact('data', 'blogs'));
     }
