528852a093283941e17226e69c4e8287abc93d32
Ferdinalaxewall <86649049+ferdinalaxewall@users.noreply.github.com>
2024-06-18 20:55:57 -0700

    Enhancement/Admin (Dashboard): Add Line and Dougnut Chart Visitor

--- app/Http/Controllers/Admin/Dashboard/DashboardController.php
+++ app/Http/Controllers/Admin/Dashboard/DashboardController.php
@@ -2,16 +2,44 @@
 
 namespace App\Http\Controllers\Admin\Dashboard;
 
+use App\Models\Tour;
 use App\Models\User;
+use App\Models\Destination;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Helpers\Utilities\RandomGenerator;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $data['count_admin'] = User::select('id')->where('role', User::ADMIN_ROLE)->count();
-        $data['count_staff'] = User::select('id')->where('role', User::STAFF_ROLE)->count();
+        // Count Data
+        $data['count_admin'] = User::where('role', User::ADMIN_ROLE)->count('id');
+        $data['count_staff'] = User::where('role', User::STAFF_ROLE)->count('id');
+        $data['count_destination'] = Destination::count('id');
+        $data['count_tour'] = Tour::count('id');
+
+        // Tour Graphic Data
+        $tourVisitors = Tour::select('title', 'total_visitor')->where('total_visitor', '>=', 1)->get();
+        $data['tour_line_graph']['categories'] = $tourVisitors->map(fn ($item) => $item->title);
+        $data['tour_line_graph']['series'] = $tourVisitors->map(fn ($item) => $item->total_visitor);
+
+        // Destination Graphic Data
+        $destinationVisitors = Destination::select('title', 'total_visitor')->where('total_visitor', '>=', 1)->get();
+        $data['destination_donut_graph'] = $destinationVisitors->map(function ($item) use ($destinationVisitors) {
+            return [
+                'title' => $item->title,
+                'total_visitor' => $item->total_visitor,
+                'percentage' => round(
+                    ($item->total_visitor / $destinationVisitors->sum('total_visitor')) * 100
+                ),
+                'color' => RandomGenerator::generateRandomColor()
+            ];
+        });
+
+        $maxPercentage = $data['destination_donut_graph']->max('percentage');
+        $data['index_max_donut_graph'] = $data['destination_donut_graph']->search(fn ($item) => $item['percentage'] == $maxPercentage);
+
         return view('admin.pages.dashboard.index', compact('data'));
     }
 }
--- resources/views/admin/pages/dashboard/index.blade.php
+++ resources/views/admin/pages/dashboard/index.blade.php
@@ -2,13 +2,15 @@
 @section('title', 'Dashboard')
 
 @section('content')
-<div class="row">
-    <div class="col-sm-6 col-md-4">
+
+<div class="row" style="row-gap: 15px;">
+@if (auth()->user()->role == \App\Models\User::ADMIN_ROLE)
+    <div class="col-sm-6 col-md-3">
         <a href="{{ route('admin.users.administrator.index') }}" class="card card-hover h-100">
             <div class="card-body">
                 <div class="d-flex justify-content-between">
                     <div class="card-info">
-                        <p class="card-text mb-2">Jumlah Administrator</p>
+                        <p class="card-text mb-2">Administrator</p>
                         <div class="d-flex align-items-end mb-2">
                             <h4 class="card-title mb-0 me-2">{{ $data['count_admin'] }}</h4>
                         </div>
@@ -23,12 +25,12 @@
         </a>
     </div>
     
-    <div class="col-sm-6 col-md-4">
+    <div class="col-sm-6 col-md-3">
         <a href="{{ route('admin.users.staff.index') }}" class="card card-hover h-100">
             <div class="card-body">
                 <div class="d-flex justify-content-between">
                     <div class="card-info">
-                        <p class="card-text mb-2">Jumlah Staff</p>
+                        <p class="card-text mb-2">Staff</p>
                         <div class="d-flex align-items-end mb-2">
                             <h4 class="card-title mb-0 me-2">{{ $data['count_staff'] }}</h4>
                         </div>
@@ -42,5 +44,355 @@
             </div>
         </a>
     </div>
+    
+    <div class="col-sm-6 col-md-3">
+        <a href="{{ route('admin.destination.index') }}" class="card card-hover h-100">
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <div class="card-info">
+                        <p class="card-text mb-2">Destinasi</p>
+                        <div class="d-flex align-items-end mb-2">
+                            <h4 class="card-title mb-0 me-2">{{ $data['count_destination'] }}</h4>
+                        </div>
+                    </div>
+                    <div class="card-icon align-self-center">
+                        <span class="badge bg-label-primary rounded p-2">
+                        <i class="bx bx-map-pin bx-sm"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    
+    <div class="col-sm-6 col-md-3">
+        <a href="{{ route('admin.tour.index') }}" class="card card-hover h-100">
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <div class="card-info">
+                        <p class="card-text mb-2">Tour</p>
+                        <div class="d-flex align-items-end mb-2">
+                            <h4 class="card-title mb-0 me-2">{{ $data['count_tour'] }}</h4>
+                        </div>
+                    </div>
+                    <div class="card-icon align-self-center">
+                        <span class="badge bg-label-primary rounded p-2">
+                        <i class="bx bx-trip bx-sm"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+@else
+    <div class="col-sm-6 col-md-4">
+        <a href="{{ route('admin.users.staff.index') }}" class="card card-hover h-100">
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <div class="card-info">
+                        <p class="card-text mb-2">Staff</p>
+                        <div class="d-flex align-items-end mb-2">
+                            <h4 class="card-title mb-0 me-2">{{ $data['count_staff'] }}</h4>
+                        </div>
+                    </div>
+                    <div class="card-icon align-self-center">
+                        <span class="badge bg-label-primary rounded p-2">
+                        <i class="bx bx-group bx-sm"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    
+    <div class="col-sm-6 col-md-4">
+        <a href="{{ route('admin.destination.index') }}" class="card card-hover h-100">
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <div class="card-info">
+                        <p class="card-text mb-2">Destinasi</p>
+                        <div class="d-flex align-items-end mb-2">
+                            <h4 class="card-title mb-0 me-2">{{ $data['count_destination'] }}</h4>
+                        </div>
+                    </div>
+                    <div class="card-icon align-self-center">
+                        <span class="badge bg-label-primary rounded p-2">
+                        <i class="bx bx-map-pin bx-sm"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    
+    <div class="col-sm-6 col-md-4">
+        <a href="{{ route('admin.tour.index') }}" class="card card-hover h-100">
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <div class="card-info">
+                        <p class="card-text mb-2">Tour</p>
+                        <div class="d-flex align-items-end mb-2">
+                            <h4 class="card-title mb-0 me-2">{{ $data['count_tour'] }}</h4>
+                        </div>
+                    </div>
+                    <div class="card-icon align-self-center">
+                        <span class="badge bg-label-primary rounded p-2">
+                        <i class="bx bx-trip bx-sm"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+@endif
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">
+                    Grafik Pengunjung Destinasi
+                </h5>
+            </div>
+            <div class="card-body">
+                <div id="donutChart"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">
+                    Grafik Pengunjung Berdasarkan Tour
+                </h5>
+            </div>
+            <div class="card-body">
+                <div id="lineChart"></div>
+            </div>
+        </div>
+    </div>
+
 </div>
+
 @endsection
+
+@push('script')
+<script>
+    const lineChartEl = document.querySelector('#lineChart'),
+    lineChartConfig = {
+        chart: {
+            height: 400,
+            type: 'line',
+            parentHeightOffset: 0,
+            zoom: {
+                enabled: false
+            },
+            toolbar: {
+                show: false
+            }
+        },
+        series: [
+            {
+                data: @json($data['tour_line_graph']['series']->toArray())
+            }
+        ],
+        markers: {
+            strokeWidth: 7,
+            strokeOpacity: 1,
+            strokeColors: [config.colors.white],
+            colors: [config.colors.primary]
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'straight'
+        },
+        colors: [config.colors.primary],
+        grid: {
+            borderColor: config.colors.borderColor,
+            xaxis: {
+                lines: {
+                show: true
+                }
+            },
+            padding: {
+                top: -20,
+            }
+        },
+        tooltip: {
+            custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+                return '<div class="px-3 py-2">' + '<span>' + series[seriesIndex][dataPointIndex] + ' Pengunjung</span>' + '</div>';
+            }
+        },
+        xaxis: {
+            categories: @json($data['tour_line_graph']['categories']->toArray()),
+            axisBorder: {
+                show: false
+            },
+            axisTicks: {
+                show: false
+            },
+            labels: {
+                style: {
+                    colors: config.colors.textMuted,
+                    fontSize: '10px'
+                }
+            }
+        },
+        yaxis: {
+            labels: {
+                style: {
+                    colors: config.colors.textMuted,
+                    fontSize: '10px'
+                }
+            }
+        }
+    };
+    if (typeof lineChartEl !== undefined && lineChartEl !== null) {
+    const lineChart = new ApexCharts(lineChartEl, lineChartConfig);
+    lineChart.render();
+    }
+</script>
+
+<script>
+    const donutChartEl = document.querySelector('#donutChart'),
+    donutChartConfig = {
+        chart: {
+            height: 390,
+            type: 'donut'
+        },
+        labels: @json($data['destination_donut_graph']->pluck('title')->toArray()),
+        series: @json($data['destination_donut_graph']->pluck('percentage')->toArray()),
+        colors: @json($data['destination_donut_graph']->pluck('color')->toArray()),
+        stroke: {
+            show: false,
+            curve: 'straight'
+        },
+        dataLabels: {
+            enabled: true,
+            formatter: function (val, opt) {
+                return parseInt(val, 10) + '%';
+            }
+        },
+        legend: {
+            show: true,
+            position: 'bottom',
+            markers: { offsetX: -3 },
+            itemMargin: {
+                vertical: 3,
+                horizontal: 10
+            },
+            labels: {
+                colors: config.colors.textMuted,
+                useSeriesColors: false
+            }
+        },
+        plotOptions: {
+            pie: {
+                donut: {
+                    labels: {
+                        show: true,
+                        name: {
+                            fontSize: '2rem',
+                            fontFamily: 'Open Sans'
+                        },
+                        value: {
+                            fontSize: '1.2rem',
+                            color: config.colors.textMuted,
+                            fontFamily: 'Open Sans',
+                            formatter: function (val) {
+                                return parseInt(val, 10) + '%';
+                            }
+                        },
+                        total: {
+                            show: true,
+                            fontSize: '1.2rem',
+                            color: config.colors.black,
+                            label: "{{ $data['destination_donut_graph'][$data['index_max_donut_graph']]['title'] }}",
+                            formatter: function (w) {
+                                return "{{ $data['destination_donut_graph'][$data['index_max_donut_graph']]['percentage'] }}%";
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        responsive: [
+            {
+                breakpoint: 992,
+                options: {
+                    chart: {
+                        height: 380
+                    },
+                    legend: {
+                        position: 'bottom',
+                        labels: {
+                            colors: config.colors.textMuted,
+                            useSeriesColors: false
+                        }
+                    }
+                }
+            },
+            {
+                breakpoint: 576,
+                options: {
+                    chart: {
+                        height: 320
+                    },
+                    plotOptions: {
+                        pie: {
+                        donut: {
+                            labels: {
+                            show: true,
+                            name: {
+                                fontSize: '1.5rem'
+                            },
+                            value: {
+                                fontSize: '1rem'
+                            },
+                            total: {
+                                fontSize: '1.5rem'
+                            }
+                            }
+                        }
+                        }
+                    },
+                    legend: {
+                        position: 'bottom',
+                        labels: {
+                        colors: config.colors.textMuted,
+                            useSeriesColors: false
+                        }
+                    }
+                }
+            },
+            {
+                breakpoint: 420,
+                options: {
+                    chart: {
+                        height: 280
+                    },
+                    legend: {
+                        show: false
+                    }
+                }
+            },
+            {
+                breakpoint: 360,
+                options: {
+                    chart: {
+                        height: 250
+                    },
+                    legend: {
+                        show: false
+                    }
+                }
+            }
+        ]
+    };
+    if (typeof donutChartEl !== undefined && donutChartEl !== null) {
+        const donutChart = new ApexCharts(donutChartEl, donutChartConfig);
+        donutChart.render();
+    }
+</script>
+@endpush
